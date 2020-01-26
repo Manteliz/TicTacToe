e7fae622f96ed2a088b5b75760b8d67ad2c5c763
Manteliz <mantasvaist@gmail.com>
2020-01-26 22:22:46 +0200

    Image update

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
 var canvas = document.getElementById("canvasGame");
 var canvasContext = canvas.getContext('2d');
 
-//draws the outer border
-canvasContext.stokeStyle = 'black';
-canvasContext.lineWidth = 15;
-canvasContext.strokeRect(0,0, canvas.width, canvas.height);
+drawBoard();
 
-//draws 2 inner horizontal and vertical lines
-canvasContext.beginPath();
-canvasContext.lineWidth = 2;
-canvasContext.moveTo(canvas.width/3, 0);
-canvasContext.lineTo(canvas.width/3, canvas.height);
-canvasContext.moveTo(2*canvas.width/3, 0);
-canvasContext.lineTo(2*canvas.width/3, canvas.height);
-canvasContext.moveTo(0, canvas.height/3);
-canvasContext.lineTo(canvas.width, canvas.height/3);
-canvasContext.moveTo(0, 2*canvas.height/3);
-canvasContext.lineTo(canvas.width, 2*canvas.height/3);
-canvasContext.stroke();
+/*
+var X = document.getElementById('X');
+var O = document.getElementById('O');
+canvasContext.drawImage(X, 200, 200);
+*/
+
+
+function drawBoard(){
+    //draws the outer border
+    canvasContext.stokeStyle = 'black';
+    canvasContext.lineWidth = 15;
+    canvasContext.strokeRect(0,0, canvas.width, canvas.height);
+
+    //draws 2 inner horizontal and vertical lines
+    canvasContext.beginPath();
+    canvasContext.lineWidth = 2;
+    canvasContext.moveTo(canvas.width/3, 0);
+    canvasContext.lineTo(canvas.width/3, canvas.height);
+    canvasContext.moveTo(2*canvas.width/3, 0);
+    canvasContext.lineTo(2*canvas.width/3, canvas.height);
+    canvasContext.moveTo(0, canvas.height/3);
+    canvasContext.lineTo(canvas.width, canvas.height/3);
+    canvasContext.moveTo(0, 2*canvas.height/3);
+    canvasContext.lineTo(canvas.width, 2*canvas.height/3);
+    canvasContext.stroke();
+}
+
 
 //creating a board object. If the region is empty then it's value will be null, if it stores O --> value = 0, if X --> value = 1
 var board = {
